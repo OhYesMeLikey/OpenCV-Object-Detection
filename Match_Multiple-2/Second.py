833a4a95a3c1d3ad79a85952649121f00b19c9b2
Leo Tan <leotan213@gmail.com>
2023-09-28 19:41:43 -0400

    Print out locations of matches that passes the threshold

--- Match_Multiple-2/Second.py
+++ Match_Multiple-2/Second.py
@@ -11,5 +11,18 @@ target_img = cv.imread("Bot.jpg", cv.IMREAD_UNCHANGED)
 
 # Search and find the target in the source file
 result = cv.matchTemplate(source_img, target_img, cv.TM_CCOEFF_NORMED)
+#print(result)
 
+# Set a threshold for the resulting confidence values and print out the locations
+threshold = 0.7
+locations = np.where(result >= threshold)
+print("locations\n", locations)
 
+# Zip up the locations into position tuples
+# The "star" symbol unpacks the list, and the "zip" keyword merges lists into new lists of each items that's at the same index
+# In this case, the "locations" list gets reversed, removes the outermost list and gives multiple 1D lists, 
+# go through each element per index and put them individually into a new list, and lastly, all the newly generated lists gets
+# combined into one large list 
+
+locations = list(zip(*locations[::-1]))
+print("zipped up locations\n", locations)
