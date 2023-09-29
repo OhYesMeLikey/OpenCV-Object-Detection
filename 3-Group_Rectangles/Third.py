c4b87bdac976f778a21e07f75257986a6ce37955
Leo Tan <leotan213@gmail.com>
2023-09-29 17:41:17 -0400

    Grouping rectangles together

---
+++ 3-Group_Rectangles/Third.py
@@ -0,0 +1,46 @@
+#! C:\Users\leota\Desktop\OpenCV Object Detection\myenv\Scripts\python.exe
+import cv2 as cv
+import numpy as np
+
+
+source_img = cv.imread("TheRange.jpg", cv.IMREAD_UNCHANGED)
+target_img = cv.imread("Bot.jpg", cv.IMREAD_UNCHANGED)
+
+target_w = target_img.shape[1]
+target_h = target_img.shape[0]
+
+result = cv.matchTemplate(source_img, target_img, cv.TM_CCOEFF_NORMED)
+
+threshold = 0.7
+locations = np.where(result >= threshold)
+locations = list(zip(*locations[::-1]))
+
+# Create a list [x,y,w,h] rectangles
+rectangles = []
+for loc in locations:
+    rect = [int(loc[0]), int(loc[1]), target_w, target_h]
+    rectangles.append(rect)
+print("Rectangles", rectangles)
+
+# Checking the truth value of a list with multiple values is ambiguous, so it was replaced
+# with checking the length of a list
+if len(rectangles):
+    print("Found good matches")
+
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
+
+    for x, y, w, h in rectangles:
+        top_left = (x, y)
+
+        bottom_right = (x + w, y + h)
+
+        cv.rectangle(source_img, top_left, bottom_right, line_color, line_type)
+
+    cv.imshow("Result", source_img)
+
+    cv.waitKey()
+
+    cv.imwrite("Result.jpg", source_img)
+else:
+    print("No good matches")
