5d994ef1bfed95ffff52a4111af957a87489b213
Leo Tan <leotan213@gmail.com>
2023-10-07 16:52:21 -0400

    Just a simple implementation of constantly taking screenshots

--- 4-Window_Capture/Fourth.py
+++ 4-Window_Capture/Fourth.py
@@ -2,20 +2,28 @@ import cv2 as cv
 import numpy as np
 import os
 import pyautogui
+from time import time
 
-# Change the working directory to the folder that this script is in.
+# Changes the working directory to the folder that this script is in.
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# Marks down the current time before entering the loop.
+loop_time = time()
 while True:
     screenshot = pyautogui.screenshot()
-    # Convert the screenshotted image and assign it back to the 'screenshot' variable.
+    # Converts the screenshotted image and assign it back to the 'screenshot' variable.
     screenshot = np.array(screenshot)
-    # Convert RGB to BGR.
+
+    # Converts RGB to BGR.
     # Without the following code, opencv will display multiple different colour screenshots.
     # screenshot = screenshot[:, :, ::-1].copy()
     screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
 
     cv.imshow("Computer Vision", screenshot)
+
+    # Prints out the FPS
+    print("FPS {}".format(1 / (time() - loop_time)))
+    loop_time = time()
 
     # Press 'q' with the output window focused to exit.
     # Waits for 1 ms every loop to check for key press.
