5881ca6d0daf62f734f8540e3bb72e7e08e73f13
Leo Tan <leotan213@gmail.com>
2023-10-07 23:21:21 -0400

    New implementation using built-in window capture gives WAY more FPS

--- 4-Window_Capture/Fourth.py
+++ 4-Window_Capture/Fourth.py
@@ -2,22 +2,48 @@ import cv2 as cv
 import numpy as np
 import os
 from time import time
-from PIL import ImageGrab
+import win32gui, win32ui, win32con
+
 
 # Changes the working directory to the folder that this script is in.
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+
+def window_capture():
+    w = 2560  # set this
+    h = 1440  # set this
+
+    # hwnd = win32gui.FindWindow(None, windowname)
+    hwnd = None
+
+    # Get the window image data
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+
+    # Save the screenshot
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype="uint8")
+    img.shape = (h, w, 4)
+
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+
+    return img
+
+
 # Marks down the current time before entering the loop.
 loop_time = time()
 while True:
-    screenshot = ImageGrab.grab()
-    # Converts the screenshotted image and assign it back to the 'screenshot' variable.
-    screenshot = np.array(screenshot)
-
-    # Converts RGB to BGR.
-    # Without the following code, opencv will display multiple different colour screenshots.
-    # screenshot = screenshot[:, :, ::-1].copy()
-    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+    # Obtains a screenshot from window_capture() that open cv can use right away
+    screenshot = window_capture()
 
     cv.imshow("Computer Vision", screenshot)
 
@@ -30,6 +56,5 @@ while True:
     if cv.waitKey(1) == ord("q") or cv.waitKey(1) == ord("Q"):
         cv.destroyAllWindows()
         break
-
 
 print("Done.")
