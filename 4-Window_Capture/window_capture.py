8632fac9737b9873dc2405bce08a704741fd5bdc
Leo Tan <leotan213@gmail.com>
2023-10-08 00:04:32 -0400

    Separated the classes and was able to capture Valorant but not a good job

---
+++ 4-Window_Capture/window_capture.py
@@ -0,0 +1,66 @@
+import numpy as np
+import win32gui, win32ui, win32con
+
+
+class WindowCapture:
+    # Class properties
+    w = 0
+    h = 0
+    hwnd = None
+
+    # Constructor
+    def __init__(self, window_name):
+        # Find the handle for the window that we want to capture
+        self.hwnd = win32gui.FindWindow(None, window_name)
+        if not self.hwnd:
+            raise Exception("Window not found: {}".format(window_name))
+
+        # get the window size
+        window_rect = win32gui.GetWindowRect(self.hwnd)
+        self.w = window_rect[2] - window_rect[0]
+        self.h = window_rect[3] - window_rect[1]
+
+    def get_screenshot(self):
+        # Get the window image data
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (0, 0), win32con.SRCCOPY)
+
+        # Save the screenshot
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype="uint8")
+        img.shape = (self.h, self.w, 4)
+
+        # Free Resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
+        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
+        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
+        img = img[..., :3]
+
+        # make image C_CONTIGUOUS to avoid errors that look like:
+        #   File ... in draw_rectangles
+        #   TypeError: an integer is required (got type tuple)
+        # see the discussion here:
+        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+        img = np.ascontiguousarray(img)
+
+        return img
+
+    # Find the name of the window you're interested in.
+    # Once you have it, update window_capture().
+    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
+    def list_window_names(self):
+        def winEnumHandler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+
+        win32gui.EnumWindows(winEnumHandler, None)
